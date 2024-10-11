1137fc2c8f0eecdc0bd224706189fb041be02f39
mlshere <mlsherezade@gmail.com>
2024-10-11 11:17:24 +0200

    Add checkMillionDollarIdea middleware

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,12 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (req, res, next) => {
+    const { numWeeks, weeklyRevenue } = req.body;
+    const totalValue = Number(numWeeks) * Number(weeklyRevenue);
+    if (totalValue >= 1000000) {
+      next();
+    } else {
+      res.status(400).send('Idea is not worth enough!');
+    }
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
